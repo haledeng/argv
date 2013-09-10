1dc5cd7b955848b9f1b02e9e2a6ce7622531aa38
hale_deng <dhlwust@163.com>
2013-09-10 14:11:31 +0800

    Create argv.js

---
+++ argv.js
@@ -0,0 +1,31 @@
+;(function(definitioin){
+    var hasExports = typeof module !== undefined && module.exports;
+    if(hasExports){
+      module.exports = definitioin();
+    }else{
+      this['args'] = definitioin();
+    }
+
+}).call(this, function(){
+    var args = {};
+    debugger;
+    args.get = function(){
+        var argv = [].slice.call(process.argv, 2),
+            i=0,
+            len = argv.length,
+            results = {};     
+        for(; i < len; i++){
+            var item = argv[i],
+                param = [];
+            if(item.indexOf('=') > -1){
+                param = item.split('=');
+                results[param[0]] = param[1];
+            }else{
+                results[item] = true;
+            }
+        }
+        return results;
+        
+    }
+    return args;
+});
